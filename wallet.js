1f07f25d9625aae0ac3e5b4454bc643a3a63ad3b
Pragmatic <cryptopragmatic@protonmail.com>
2021-03-18 11:12:28 +0100

    login work

--- wallet.js
+++ wallet.js
@@ -1,68 +1,11 @@
-import $ from "jquery";
-var activeScreen = "home";
-$("#receive").on("click", function () {
-    activeScreen = "receive";
-    $("#screen-home").fadeOut(function () {
-        $("#screen-receive").fadeIn();
-    });
-});
-$("#backFromReceive").on("click", function () {
-    activeScreen = "home";
-    $("#screen-receive").fadeOut(function () {
-        $("#screen-home").fadeIn();
-    });
-});
-$("#send").on("click", function () {
-    activeScreen = "send";
-    $("#screen-home").fadeOut(function () {
-        $("#screen-send").fadeIn();
-    });
-});
-$("#backFromSend").on("click", function () {
-    activeScreen = "home";
-    $("#screen-send").fadeOut(function () {
-        $("#screen-home").fadeIn();
-    });
-});
-$("#cards").on("click", function () {
-    activeScreen = "cards";
-    $("#screen-home").fadeOut(function () {
-        $("#screen-cards").fadeIn();
-    });
-});
-$("#backFromCards").on("click", function () {
-    activeScreen = "home";
-    $("#screen-cards").fadeOut(function () {
-        $("#screen-home").fadeIn();
-    });
-});
-$("#settings").on("click", function () {
-    if (activeScreen != "home") {
-        $("#screen-" + activeScreen).fadeOut(function () {
-            $("#screen-settings").fadeIn();
-            activeScreen = "settings";
-        });
+"use strict";
+class Wallet {
+    constructor() {
+        this.address = "";
+        this.seed = "";
     }
-    else {
-        activeScreen = "settings";
-        $("#screen-home").fadeOut(function () {
-            $("#screen-settings").fadeIn();
-        });
+    getAddress() {
+        return this.address;
     }
-});
-$("#backFromSettings").on("click", function () {
-    activeScreen = "home";
-    $("#screen-settings").fadeOut(function () {
-        $("#screen-home").fadeIn();
-    });
-});
-document.addEventListener('DOMContentLoaded', (event) => {
-    $("#page-loading").fadeOut(function () {
-        var page = getPage();
-        $("#page-" + page).fadeIn();
-    });
-});
-function getPage() {
-    return "main";
 }
 //# sourceMappingURL=wallet.js.map
